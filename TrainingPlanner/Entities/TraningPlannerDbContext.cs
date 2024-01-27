468ffb6d6232fa1ef02ec7d3a4039f8811f39fe1
Karolina Mikolajczyk <karolina.kujawska@interia.pl>
2024-01-27 14:47:40 +0100

    addEFCore

---
+++ TrainingPlanner/Entities/TraningPlannerDbContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace WebApplication1.Entities;
+
+public class TrainingPlannerDbContext: DbContext
+
+{
+    private string _connectionString = String.Empty;
+    protected TrainingPlannerDbContext(DbSet<Exercises> exercises)
+    {
+        Exercises = exercises;
+    }
+
+    public DbSet<Exercises> Exercises { get; set; }
+
+
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Exercises>();
+
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder contextOptionsBuilder)
+    {
+        contextOptionsBuilder.UseSqlServer(_connectionString);
+    }
+}
